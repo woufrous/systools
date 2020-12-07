559a1ed52f6af30b572a09474c47c57db421b1fc
Hans Grob <H.Grob@ifndef.de>
2020-12-07 10:07:49 +0100

    factor out file reading

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::fs;
 use std::option::Option;
 use std::vec::Vec;
+use std::path;
+use std::io;
+
+fn read_num_from_file<NumT: std::str::FromStr>(fpath: &path::Path) -> io::Result<NumT> {
+    let data = match fs::read_to_string(fpath) {
+        Err(err) => return Err(err),
+        Ok(val) => val,
+    };
+    match data.trim().parse::<NumT>() {
+        Err(_) => Err(io::Error::new(io::ErrorKind::InvalidData, "number can't be parsed")),
+        Ok(val) => Ok(val),
+    }
+}
 
 fn get_cpu_speed(idx: u8) -> Option<u32> {
     let fpath = format!("/sys/devices/system/cpu/cpu{}/cpufreq/scaling_cur_freq", idx);
+    let fpath = path::Path::new(&fpath);
 
-    match fs::read_to_string(fpath) {
+    match read_num_from_file::<u32>(fpath) {
         Err(_) => None,
-        Ok(val) => match val.trim().parse::<u32>() {
-            Err(_) => None,
-            Ok(val) => Some(val),
-        },
+        Ok(val) => Some(val)
     }
 }
 
